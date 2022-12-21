6ef25096b6dae15f66adfbd46b6cabca9f9179f2
araujoleonardo <araujoleonardo.ti@gmail.com>
2022-12-21 12:26:23 -0400

    Atualização

--- database/migrations/2022_12_21_132320_create_disciplinas_professores_table.php
+++ database/migrations/2022_12_21_132320_create_disciplinas_professores_table.php
@@ -14,6 +14,7 @@ class CreateDisciplinasProfessoresTable extends Migration
     public function up()
     {
         Schema::create('disciplinas_professores', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('professores_id')->constrained();
             $table->foreignId('disciplinas_id')->constrained();
             $table->timestamps();
